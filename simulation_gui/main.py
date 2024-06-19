a4886d556b1f5ed898ec8d5e9687d41976732854
Alberto Nidasio <nidasioalberto@gmail.com>
2024-06-19 19:10:02 +0200

    GUI: Added more arguments (`cols`, `rows` and `cell_size`)

--- simulation_gui/main.py
+++ simulation_gui/main.py
@@ -8,21 +8,25 @@ from PySide6.QtWidgets import QApplication
 from components.Map import MapWidget, MapEditorWidget
 
 parser = argparse.ArgumentParser(description='Optional app description')
-parser.add_argument("mode", choices=["visualizer", "editor"], default="editor")
+parser.add_argument(
+    "--mode", choices=["visualizer", "editor"], default="visualizer")
 parser.add_argument("--map_file", type=argparse.FileType("r"),
                     default="examples/simple_map.json", help="Map file to visualize")
-
+parser.add_argument("--cols", type=int, default=10, help="Number of columns")
+parser.add_argument("--rows", type=int, default=10, help="Number of rows")
+parser.add_argument("--cell_size", type=int, default=50,
+                    help="Size, in pixels, of each cell")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     app = QApplication([])
 
     if args.mode == "visualizer":
-        map = MapWidget(10, 10)
+        map = MapWidget(args.cols, args.rows, args.cell_size)
         map.clear()
         map.draw_map(json.load(args.map_file))
     elif args.mode == "editor":
-        map = MapEditorWidget(10, 10)
+        map = MapEditorWidget(args.cols, args.rows, args.cell_size)
         map.clear()
 
     map.show()
